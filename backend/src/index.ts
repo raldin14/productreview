f979858447d99f839ed03f101b49e8017674cdf8
raldin14 <raldin.hidalgo@gmail.com>
2025-05-06 21:55:48 -0500

    Finishing product call

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import path from 'path';
-
+import productRoutes from './routes/productRoutes';
+import reviewRoutes from './routes/reviewRoutes';
+import errorHandler from './middlewares/errorHandler';
 
 const app = express();
 const PORT = 3000;
@@ -9,8 +11,10 @@ app.use(express.json());
 // serve static images
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-// app.use('/products');
-// app.use('/products');
+app.use('/products',productRoutes);
+app.use('/products',reviewRoutes);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
