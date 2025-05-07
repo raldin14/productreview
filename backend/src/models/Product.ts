8775e6ad66efb9355d96f418d96aa30aaea0c562
raldin14 <raldin.hidalgo@gmail.com>
2025-05-06 20:50:38 -0500

    Adding product model

---
+++ backend/src/models/Product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+    id: string;
+    name: string;
+    description: string;
+    category: string;
+    price: number;
+    dateAdded: string;
+    averageRating?: number;
+    image?: string;
+}
