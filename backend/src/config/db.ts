8ba631ae04257923be989560528db0179d121ba4
raldin14 <raldin.hidalgo@gmail.com>
2025-05-15 07:53:03 -0500

    Working with enviroment

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 
 const connectDB =async () => {
+    const env = process.env.NODE_ENV || 'development';
+
+    const mongoURI = env === 'production' ? process.env.MONGO_URI : process.env.MONGO_URI_DEV;
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI as string);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        const conn = await mongoose.connect(mongoURI as string);
+        console.log(`MongoDB Connected: ${conn.connection.host} : ${env}`);
     } catch (error) {
         console.error("MongoDB connection feiled: ", error);
         process.exit(1);
