5f7d827ac0cfb1635f93b86fc3e67cd28fd818cc
raldin14 <raldin.hidalgo@gmail.com>
2025-05-14 22:48:01 -0500

    changing port

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB =async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/productCatalogDB");
+        const conn = await mongoose.connect(process.env.MONGO_URI as string);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error("MongoDB connection feiled: ", error);
